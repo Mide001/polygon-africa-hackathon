e82e83a0bcf4edc3ab5cfd44a6e472a3e9ecf8b9
Mide001 <olamideisreal5@gmail.com>
2022-12-03 16:57:06 +0100

    updated

--- frontend-app/src/function/fetchIPFS.js
+++
@@ -1,6 +0,0 @@
-export const fetchIPFS = async ({ tokenURI }) => {
-    const metadata = await fetch(tokenURI);
-    const metadataJSON = await metadata.json();
-    console.log(metadataJSON);
-    return JSON;
-};
